00e092a5c7260f814466eccfa75f02797e863ba4
Zaviere Washington <159487623+KrAceZ@users.noreply.github.com>
2024-10-21 18:46:25 -0400

    Update web-scrape.py

--- web-scrape.py
+++ web-scrape.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import pandas as pd
 
-driver = webdriver.Chrome() #wouldn't work with filepath filled. StackOverflow thread that said to just leave it empty past a certain version of selenium 
+driver = webdriver.Chrome()
 
 products=[] #List to store name of the product
 prices=[] #List to store price of the product
@@ -10,17 +10,16 @@ ratings=[] #List to store rating of the product
 driver.get("https://www.flipkart.com/search?sid=b5g&otracker=CLP_Filters&p%5B%5D=facets.brand%255B%255D%3DHP&p%5B%5D=facets.brand%255B%255D%3DASUS&p%5B%5D=facets.brand%255B%255D%3DLenovo")
 
 content = driver.page_source
-soup = BeautifulSoup(content)
-# for a in soup.findAll('a',href=True, attrs={'class':'_1YokD2 _3Mn1Gg'}):
-for a in soup.findAll('div', attrs={'class':'_1YokD2 _3Mn1Gg'}):
-    name=a.find('div', attrs={'class':'_4rR01T'})
-    price=a.find('div', attrs={'class':'Nx9bqj'})
-    rating=a.find('div', attrs={'class':'_3LWZlK'})
+soup = BeautifulSoup(content, 'html.parser')
+for a in soup.findAll('div', attrs={'class':'_75nlfW'}):
+    name = a.find('a', attrs={'class':'wjcEIp'})
+    price = a.find('div', attrs={'class':'Nx9bqj'})
+    rating = a.find('div', attrs={'class':'XQDdHH'})
     products.append(name.text)
     prices.append(price.text)
     ratings.append(rating.text)
 
-df = pd.DataFrame({'Product Name':products,'Price':prices,'Rating':ratings})
+df = pd.DataFrame({'Product Name':products, 'Price':prices, 'Rating':ratings})
 df.to_csv('products.csv', index=False, encoding='utf-8')
 
 driver.close()
